2200763e9d608b94f27842878a411ccae920b4d9
Cocotrilo <nogalesisaac25@gmail.com>
2026-09-13 20:52:00 -0500

    Initial Commit

---
+++ 1test.py
@@ -0,0 +1,8 @@
+
+def isPair (pair):
+    if pair % 2 == 0:
+        print("Is pair")
+    else:
+        print("not pair")
+
+isPair(4)
---
+++ faceblur.py
@@ -0,0 +1,5 @@
+import cv2
+
+imagen = cv2.imread("foto.jpeg")
+
+print(imagen.shape)
