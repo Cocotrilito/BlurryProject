a1f234c0c375a8c6f78ce1e72c134bdbd7ef62d9
Cocotrilo <nogalesisaac25@gmail.com>
2026-09-13 21:21:01 -0500

    Added the blur hope it works gng

--- faceblur.py
+++ faceblur.py
@@ -1,6 +1,17 @@
 import cv2
 
-imagen = cv2.imread("foto.jpeg")
+image = cv2.imread("foto.jpeg")
 
-clasifier = cv2.CascadeClassfier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-print(imagen.shape)
+clasifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = clasifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+print(faces)
+
+for (x, y, w, h) in faces:
+    region = image[y:y+h, x:x+w]
+    region_blurred = cv2.GaussianBlur(region, (99,99), 30)
+    image[y:y+h, x:x+w] = region_blurred
+
+cv2.imwrite("result.jpg", image)
