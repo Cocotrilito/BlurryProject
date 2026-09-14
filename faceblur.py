f314f0d24a4b1848a37191c9034b70de5d6d3469
Cocotrilo <nogalesisaac25@gmail.com>
2026-09-13 22:16:47 -0500

    Replace square blur with oval mask using numpy

--- faceblur.py
+++ faceblur.py
@@ -1,17 +1,21 @@
 import cv2
+import numpy as np
 
 image = cv2.imread("foto.jpeg")
 
-clasifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-faces = clasifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+faces = classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 print(faces)
 
 for (x, y, w, h) in faces:
     region = image[y:y+h, x:x+w]
     region_blurred = cv2.GaussianBlur(region, (99,99), 30)
-    image[y:y+h, x:x+w] = region_blurred
+    mask = np.zeros((h, w), dtype=np.uint8)
+    cv2.ellipse(mask, (w // 2, h // 2), (w // 2, h // 2), 0, 0, 360, 255, -1)
+    mask_3d = cv2.merge([mask, mask, mask])
+    image[y:y+h, x:x+w] = np.where(mask_3d == 255, region_blurred, region)
 
 cv2.imwrite("result.jpg", image)
