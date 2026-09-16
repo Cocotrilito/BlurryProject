8fcaef26afee16ca8331dbab55851d41f121186a
Cocotrilo <nogalesisaac25@gmail.com>
2026-09-16 18:10:29 -0500

    Added v2 to streamtlit

--- app.py
+++ app.py
@@ -1,31 +1,53 @@
 import streamlit as st
 import numpy as np
 import cv2
+import mediapipe as np
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from PIL import Image
+
+
+
 
 
 st.title("Face Blur Tool")
 st.write("Upload a photo and I'll blur the faces automatically")
 file = st.file_uploader("Choose a photo")
 
-from PIL import Image
+
+FACE_OVAL_INDICES = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288,
+                      397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136,
+                      172, 58, 132, 93, 234, 127, 162, 21, 54, 103, 67, 109]
+
 
 if file is not None:
     image_pil = Image.open(file)
     image_array = np.array(image_pil)
     image = cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR)
     
+    base_options = python.BaseOptions(model_asset_path='face_landmarker.task')
+    options = vision.FaceLandmarkerOptions(base_options=base_options, num_faces=5)
+    detector = vision.FaceLandmarker.create_from_options(options)
 
-    classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-    print(faces)
-    for (x, y, w, h) in faces:
-        region = image[y:y+h, x:x+w]
-        region_blurred = cv2.GaussianBlur(region, (99,99), 30)
-        mask = np.zeros((h, w), dtype=np.uint8)
-        cv2.ellipse(mask, (w // 2, h // 2), (w // 2, h // 2), 0, 0, 360, 255, -1)
-        mask_3d = cv2.merge([mask, mask, mask])
-        image[y:y+h, x:x+w] = np.where(mask_3d == 255, region_blurred, region)
-    st.image(image, channels="BGR")
-    result = cv2.imencode(".png", image)[1].tobytes()
-    st.download_button("Download blurred photo", result, "result.png", "image/png")
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_array)
+    result = detector.detect(mp_image)
+
+    height, width, _ = image.shape
+    mask = np.zeros((height, width), dtype=np.uint8)
+
+    for face_landmarks in result.face_landmarks:
+        contour_points = []
+        for index in FACE_OVAL_INDICES:
+            point = face_landmarks[index]
+            x = int(point.x * width)
+            y = int(point.y * height)
+            contour_points.append([x, y])
+        puntos_array = np.array(contour_points, dtype=np.int32)
+        cv2.fillPoly(mask, [puntos_array], 255)
+
+    image_blurred = cv2.GaussianBlur(image, (99,99), 30)
+    mask_3d = cv2.merge([mask, mask, mask])
+    result = np.where(mask_3d == 255, image_blurred, image)
+    st.image(result, channels="BGR")
+    resultBytes = cv2.imencode(".png", result)[1].tobytes()
+    st.download_button("Download blurred photo", resultBytes, "result.png", "image/png")
